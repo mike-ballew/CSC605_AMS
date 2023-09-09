73fe0153160aa3ce34e4932ff67c2b9990b654b5
Matthew Rivera <mjrivera0613dev@gmail.com>
2023-09-08 19:43:10 -0700

    Added method to update cell, made some methods more general

--- src/main/java/com/example/ams/ApplianceManager.java
+++ src/main/java/com/example/ams/ApplianceManager.java
@@ -26,16 +26,16 @@ public class ApplianceManager {
         }
     }
 
-    public Boolean removeAppliance(int primaryKeyValue) {
+    public Boolean removeAppliance(int primaryKeyValue, String primaryKeyColumnName) {
         try (Connection connection = DriverManager.getConnection(DATABASE_URL);
              Statement statement = connection.createStatement()) {
-            String deleteRowSQL = "DELETE FROM " + TABLE_NAME + " WHERE ApplianceID = '" + primaryKeyValue + "'";
+            String deleteRowSQL = "DELETE FROM " + TABLE_NAME + " WHERE " + primaryKeyColumnName + " = '" + primaryKeyValue + "'";
             boolean rowDeleted = statement.executeUpdate(deleteRowSQL) > 0;
             if (rowDeleted) {
-                System.out.println("Row with ApplianceID = " + primaryKeyValue + " deleted successfully.");
+                System.out.println("Row with " + primaryKeyColumnName + " = " + primaryKeyValue + " deleted successfully.");
                 return true;
             } else {
-                System.out.println("No rows were deleted. Row with ApplianceID = " + primaryKeyValue + " not found.");
+                System.out.println("No rows were deleted. Row with " + primaryKeyColumnName + " = " + primaryKeyValue + " not found.");
                 return false;
             }
         } catch (SQLException e) {
@@ -44,11 +44,11 @@ public class ApplianceManager {
         }
     }
 
-    public String getCellValue(String columnName, int primaryKeyValue) {
+    public String getCellValue(String primaryKeyColumnName, String columnName, int primaryKeyValue) {
         String cellValue = null;
         try (Connection connection = DriverManager.getConnection(DATABASE_URL);
              Statement statement = connection.createStatement()) {
-            String selectSQL = "SELECT " + columnName + " FROM " + TABLE_NAME + " WHERE ApplianceID = '" + primaryKeyValue + "'";
+            String selectSQL = "SELECT " + columnName + " FROM " + TABLE_NAME + " WHERE " + primaryKeyColumnName + " = '" + primaryKeyValue + "'";
             ResultSet resultSet = statement.executeQuery(selectSQL);
             if (resultSet.next()) {
                 cellValue = resultSet.getString(columnName);
@@ -58,17 +58,42 @@ public class ApplianceManager {
         }
         return cellValue;
     }
+    public Boolean updateCellValue(String columnName, String primaryKeyColumn, int primaryKeyValue, String newValue) {
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL);
+             Statement statement = connection.createStatement()) {
 
+            // SQL command to update the cell value
+            String updateSQL = "UPDATE " + TABLE_NAME + " SET " + columnName + " = '" + newValue + "'" + " WHERE " + primaryKeyColumn + " = '" + primaryKeyValue + "'";
+
+            // Execute the update command
+            int rowsUpdated = statement.executeUpdate(updateSQL);
+
+            if (rowsUpdated > 0) {
+                System.out.println("Cell value updated successfully.");
+                return true;
+            } else {
+                System.out.println("No rows were updated. Row with " + primaryKeyColumn + " = " + primaryKeyValue + " not found.");
+                return false;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error: " + e.getMessage());
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
         ApplianceManager manager = new ApplianceManager();
 
         String searchCol = "Alias";
+        String pKeyColName = "ApplianceID";
+        String newValue = "Chubbier Tom";
         for (int i = 0; i < 100; i++) {
             //manager.addAppliance(i, "Washer", "Unit 10" + String.valueOf(i), "HOLD" , "Good", "09/05/2023", "Chubby Tom");
-            String result = manager.getCellValue(searchCol, i);
-            System.out.println(result);
-            //manager.removeAppliance(i);
+            //String result = manager.getCellValue(pKeyColName, searchCol, i);
+            //System.out.println(result);
+            //manager.removeAppliance(i, pKeyColName);
+            manager.updateCellValue(searchCol, pKeyColName, i, newValue);
         }
     }
 }
